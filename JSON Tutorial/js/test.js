50e218a5ff7c5cee3aa91de5e46342f5a7cb683a
Suvro Debdas <95747469+Suvro-Debdas@users.noreply.github.com>
2022-08-02 11:27:11 +0530

    Add files via upload

---
+++ JSON Tutorial/js/test.js
@@ -0,0 +1,15 @@
+// Converting javascript object to json format
+
+// let student = {
+//     fname: "Suvro",
+//     lname: "Debdas",
+//     age: 28,
+// };
+// let ans = JSON.stringify(student);
+// console.log(ans);
+
+// Converting json fromat to javascript object
+
+// let student = '{"name":"Suvro","age":28}';
+// let jstudent = JSON.parse(student);
+// console.log(jstudent);
